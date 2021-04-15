203b520b5a0135506bcdd00bb7e2cebaf95bdbec
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-04-14 17:02:30 -0700

    push role data, add team member role selector

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function askUser() {
         });
 }
 // MANAGER
-function addTeamManager() {
+function addManager() {
     inquirer.prompt([{
         type: "input",
         name: "name",
@@ -45,52 +45,104 @@ function addTeamManager() {
     },
     ])
 
-
-
-    // ENGINEER
-    function addEngineer() {
-        inquirer.prompt([{
-            type: "input",
-            name: "name",
-            message: "What is the engineer's name?",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is the engineer's employee ID?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is the engineer's email address?",
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "What is the engineer's github handle?",
-        },
-        ])
-
-        // INTERN
-        function addIntern() {
-            inquirer.prompt([{
-                type: "input",
-                name: "name",
-                message: "What is the intern's name?",
-            },
-            {
-                type: "input",
-                name: "id",
-                message: "What is the intern's employee ID?",
-            },
-            {
-                type: "input",
-                name: "email",
-                message: "What is the intern's email address?",
-            },
-            {
-                type: "input",
-                name: "school",
-                message: "What school does the intern attend?",
-            },
-            ])
+        // push Manager Data
+        .then(function (data) {
+            let name = data.name;
+            let id = data.id;
+            let email = data.email;
+            let officeNum = data.officeNum;
+            let teammate = new Manager(name, id, email, officeNum); // follow up on ID
+            myTeamArray.push(teammate);
+            addTeammates();
+        });
+}
+// TEAM BUILDER
+function addTeammates() {
+    inquirer.prompt([{
+        type: "list",
+        message: "Are there more team members to add?",
+        choices: ["Add an Engineer", "Add an Intern", "No. Complete my team!"],
+        name: "addTeamData",
+    },])
+        .then(function (data) {
+            switch (data.addTeamData) {
+                case "Add an Engineer":
+                    addEngineer();
+                    break;
+                case "Add an Intern":
+                    addIntern();
+                    break;
+                case "No. Complete my team!":
+                    buildMyTeam();
+                    break;
+            }
+        });
+}
+// ENGINEER
+function addEngineer() {
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's employee ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github handle?",
+    },
+    ])
+        //push Engineer data
+        .then(function (data) {
+            let name = data.name;
+            let id = data.id;
+            let email = data.email;
+            let github = data.github;
+            let teammate = new Engineer(name, id, email, github);
+            myTeamArray.push(teammate);
+            addTeammates();
+        });
+}
+// INTERN
+function addIntern() {
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's employee ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school does the intern attend?",
+    },
+    ])
+        // push intern data
+        .then(function (data) {
+            let name = data.name;
+            let id = data.id;
+            let email = data.email;
+            let school = data.school;
+            let teammate = new Intern(name, id, email, school);
+            myTeamArray.push(teammate);
+            addTeammates();
+        });
+}
